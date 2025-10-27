b35c907abe935ddbb632a38dfa0b65e087f78883
gsousav <gssousav@gmail.com>
2025-10-27 11:14:32 +0100

    assets: add recipe images

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -5,6 +5,32 @@ class Recipe {
   Recipe(
       this.label,
       this.imageUrl,
-      );
-  // TODO: Add List<Recipe> here
+  );
+  // Recipe List
+static List<Recipe> samples = [
+  Recipe(
+      'Spaghetti and Meatballs',
+      'assets/2126711929_ef763de2b3_w.jpg',
+  ),
+  Recipe(
+    'Tomato Soup',
+    'assets/image_file'
+  ),
+  Recipe(
+    'Grilled Cheese',
+    'assets/image_file'
+  ),
+  Recipe(
+    'Chocolate Chip Cookies',
+    'assets/image_file',
+  ),
+  Recipe(
+    'Taco Salad',
+    'assets/image_file',
+  ),
+  Recipe(
+    'Hawaiian Pizza',
+    'assets/image_file'
+  ),
+  ];
 }
